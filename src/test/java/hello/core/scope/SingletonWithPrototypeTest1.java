582c2bc51e574d815b240f4d0f865819bedce0ea
David Yoon <qqpo12@naver.com>
2025-03-17 15:22:48 +0900

    Use the provider
    * Used the ObjectProvider instead of ApplicationContext to DL(Dependency LookUp)

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,8 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -38,18 +40,17 @@ public class SingletonWithPrototypeTest1 {
 
         int count2 = clientBean2.logic();
 
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private ObjectProvider<PrototypeBean> provider;
 
         public int logic() {
+            PrototypeBean prototypeBean = provider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
