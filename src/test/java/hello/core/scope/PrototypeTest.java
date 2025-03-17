f392a0d025f75a866201fe06bfd1078114041492
David Yoon <qqpo12@naver.com>
2025-03-17 14:45:08 +0900

    Add tests
    * Added tests for the Spring Scope
    * Used the singleton and prototype scope to check how to execute bean with lifeCycle

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean singletonBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean singletonBean2 = ac.getBean(PrototypeBean.class);
+
+        Assertions.assertThat(singletonBean1).isNotSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("Singleton.destroy");
+        }
+    }
+
+}
